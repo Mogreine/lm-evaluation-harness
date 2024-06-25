90332530cedf4f86a87a9179e87e659c2d853793
Egor Spirin <egor.spirin@vk.team>
2024-06-26 02:50:22 +0300

    Continuation now similar to other mmlu datasets

--- lm_eval/tasks/mmlu_ru_mera/utils.py
+++ lm_eval/tasks/mmlu_ru_mera/utils.py
@@ -12,18 +12,18 @@ def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
 
 
 def process_docs_continuation(dataset: datasets.Dataset) -> datasets.Dataset:
+    QUERY = """Ниже приведены вопросы с несколькими вариантами ответов и одним правильным на тему {subject}.
+Вопрос: {text}
+Ответ:"""
     option_keys = ["option_a", "option_b", "option_c", "option_d"]
-    outputs = ["A", "B", "C", "D"]
+    targets = ["A", "B", "C", "D"]
 
     def _process_doc(doc):
-        instruction = doc["instruction"]
         inputs = doc["inputs"]
-        choices = [out + " " + inputs[key] for out, key in zip(outputs, option_keys)]
-        output = choices[outputs.index(doc["outputs"])]
         return {
-            "query": instruction.format(**inputs),
-            "label": output,
-            "choices": choices,
+            "query": QUERY.format(subject=inputs["subject"], text=inputs["text"]),
+            "choices": [inputs[it] for it in option_keys],
+            "label": targets.index(doc["outputs"]),
         }
 
     return dataset.map(_process_doc)
